e47cf5cea3f30e2ed2a6f6a47f21f357e7e5befc
Hattori11 <guirc6@gmail.com>
2025-03-25 18:47:14 +0000

    Terminei o trabalho Batalha Naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,7 +2,12 @@
 
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+// Siga os co
+#define line 10
+#define column 10
+
+#define skill_line 5 
+#define skill_column 5
 
 int main() {
     // Nível Novato - Posicionamento dos Navios
@@ -35,6 +40,235 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
+    int board[line][column] = { // Tabuleiro
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 }
+    };
+
+    for (int i = 0; i < line; i++) {
+        for (int j = 0; j < column; j++){
+            if (i == 7 && j > 1 && j < 5) { // Navio Horizontal
+                board[7][2] = 3;
+                board[7][3] = 3;
+                board[7][4] = 3;
+            }
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < line; i++) {
+        for (int j = 0; j < column; j++){
+            if (j == 9 && i > 0 && i < 4) { // Navio Vertical
+                board[1][9] = 3;
+                board[2][9] = 3;
+                board[3][9] = 3;
+            }
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < line; i++) {
+        for (int j = 0; j < column; j++){
+            if ((i >= 1 && i <= 3) && (j >= 1 && j <= 3)) { // Navio Diagonal 1
+                board[1][1] = 3;
+                board[2][2] = 3;
+                board[3][3] = 3; 
+            }
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < line; i++) {
+        for (int j = 0; j < column; j++){
+            if ((i >= 8 && i <= 10) && (j >= 8 && j <= 10)) { // Navio Diagonal 2
+                board[7][9] = 3;
+                board[8][8] = 3;
+                board[9][7] = 3; 
+            }
+        }
+        printf("\n");
+    }
+    // Habilidade Cone
+    for (int i = 0; i < line; i++) {
+        for (int j = 0; j < column; j++) {
+            if ((i == 0 && j == 4)) {
+                board[i][j] = 5;
+            }
+            if (i == 1 && (j > 2 && j < 6)) {
+                board[i][j] = 5;
+            }
+            if (i == 2 && (j > 1 && j < 7)) {
+                board[i][j] = 5;
+            }
+        }
+    }
+    // Habilidade Cruz
+    for (int i = 0; i < line; i++) {
+        for (int j = 0; j < column; j++) {
+            if (i == 6 && (j > 4 && j < column)) {
+                board[i][j] = 5;
+            }
+            if (j == 7 && (i > 3 && i < 9)) {
+                board[i][j] = 5;
+            }
+        }
+    }
+    // Habilidade Octaedro
+    for (int i = 0; i < line; i++) {
+        for (int j = 0; j < column; j++) {
+            if (i == 5 && j == 2) {
+                board[i][j] = 5;
+            }
+            if (i == 6 && (j > 0 && j < 4)) {
+                board[i][j] = 5;
+            }
+            if (i == 7 && (j >= 0 && j < 5)) {
+                board[i][j] = 5;
+            }
+            if (i == 8 && (j > 0 && j < 4)) {
+                board[i][j] = 5;
+            }
+            if (i == 9 && j == 2) {
+                board[i][j] = 5;
+            }
+        }
+    }
+
+    printf("|  BATALHA NAVAL  |\n");
+    for (char i = 'A'; i < 'K'; i++) {
+        printf("%c ", i);
+    }
+    printf("\n");
+    int e = 1;
+    for (int i = 0; i < line; i++) {
+        for (int j = 0; j < column; j++) {
+            printf("%d ", board[i][j]); // Tabuleiro já com os navios e com as habilidades já lançadads
+        }
+        printf("%d", e);
+        e++;
+        printf("\n");
+    }
+    printf("\n");
+    // Habilidades
+    // Habilidade Octaedro
+    printf("Demonstração da Habilidade Octaedro: \n");
+    int octahedron[skill_line][skill_column];
+    for (int i = 0; i < skill_line; i++) {
+        for (int j = 0; j < skill_column; j++) {
+            octahedron[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < skill_line; i++) {
+        for (int j = 0; j < skill_column; j++) {
+            printf("%d ", octahedron[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (int i = 0; i < skill_line; i++) {
+        for (int j = 0; j < skill_column; j++) {
+            if (i == 0 && j == 2) {
+                octahedron[i][j] = 5;
+            }
+            if (i == 1 && (j > 0 && j < 4)) {
+                octahedron[i][j] = 5;
+            }
+            if (i == 2) {
+                octahedron[i][j] = 5;
+            }
+            if (i == 3 && (j > 0 && j < 4)) {
+                octahedron[i][j] = 5;
+            }
+            if (i == 4 && j == 2) {
+                octahedron[i][j] = 5;
+            }
+        }
+    }
+    for (int i = 0; i < skill_line; i++) {
+        for (int j = 0; j < skill_column; j++) {
+            printf("%d ", octahedron[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // Habilidade Cruz
+    printf("Demonstração da Habilidade Cruz: \n");
+    int cross[skill_line][skill_column];
+    for (int i = 0; i < skill_line; i++) {
+        for (int j = 0; j < skill_column; j++) {
+            cross[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < skill_line; i++) {
+        for (int j = 0; j < skill_column; j++) {
+            printf("%d ", cross[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (int i = 0; i < skill_line; i++) {
+        for (int j = 0; j < skill_column; j++) {
+            if (i == 2) {
+                cross[i][j] = 5;
+            }
+            if (j == 2) {
+                cross[i][j] = 5;
+            }
+        }
+    }
+    for (int i = 0; i < skill_line; i++) {
+        for (int j = 0; j < skill_column; j++) {
+            printf("%d ", cross[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // Habilidade Cone
+    printf("Demonstração da Habilidade Cone: \n");
+    int cone[skill_line][skill_column];
+    for (int i = 0; i < skill_line; i++) {
+        for (int j = 0; j < skill_column; j++) {
+            cone[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < skill_line; i++) {
+        for (int j = 0; j < skill_column; j++) {
+            printf("%d ", cone[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (int i = 0; i < skill_line; i++) {
+        for (int j = 0; j < skill_column; j++) {
+            if ((i == 0 && j == 2)) {
+                cone[i][j] = 5;
+            }
+            if (i == 1 && (j > 0 && j < 4)) {
+                cone[i][j] = 5;
+            }
+            if (i == 2) {
+                cone[i][j] = 5;
+            }
+        }
+    }
+    for (int i = 0; i < skill_line; i++) {
+        for (int j = 0; j < skill_column; j++) {
+            printf("%d ", cone[i][j]);
+        }
+        printf("\n");
+    }
+    
+
 
     return 0;
 }
